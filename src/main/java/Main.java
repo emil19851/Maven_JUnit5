6c2e033bafcd2e217f49c55b75fe9792b584197f
Emil <emllar-5@student.ltu.se>
2022-04-02 19:15:40 +0200

    Added calculations to main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Calculator calculator = new Calculator();
+        System.out.println("Two plus one equals: " + calculator.add(2,1));
+        System.out.println("Four minus two equals: " + calculator.subtract(4,2));
     }
+
+
 }
