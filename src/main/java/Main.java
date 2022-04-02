24908387f3888a376471f6cfba2a79a112aff415
Emil <emllar-5@student.ltu.se>
2022-04-02 23:33:14 +0200

    Moved logic from main method to new method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        show();
+    }
+
+    private static void show() {
         Calculator calculator = new Calculator();
         System.out.println("Two plus one equals: " + calculator.add(2,1));
         System.out.println("Four minus two equals: " + calculator.subtract(4,2));
